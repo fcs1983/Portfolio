acf1fa24243ef1d766f8ed0a0deb544815fbcf5f
Fábio Costa <32082233+fcs1983@users.noreply.github.com>
2017-09-18 23:58:26 -0300

    Add files via upload

---
+++ Technical test/C#/Test/Test/Question1.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+
+namespace Test
+{
+    public class GetReviews
+    {
+        string company_url;
+        int    page;
+        string public_key;
+        string private_key;
+
+        public GetReviews()
+        {
+            company_url = "test.trustedcompany.com";
+            page = 0;
+            public_key = "d43d6dbe8a04eb719fe56b409543f174";
+            private_key = "a68d2d878761ec390a0d91a70cc7a056";
+        }
+
+        public GetReviews(string company_url, int page, string public_key, string private_key)
+        {
+            this.company_url = company_url;
+            this.page = page;
+            this.public_key = public_key;
+            this.private_key = private_key;
+        }
+
+        private string GerarHash()
+        {
+            var data = Encoding.UTF8.GetBytes(public_key + company_url);
+            var key = Encoding.UTF8.GetBytes(private_key);
+            var hmac = new HMACMD5(key);
+            var hashbytes = hmac.ComputeHash(data);
+
+            return System.BitConverter.ToString(hashbytes).Replace("-", "").ToLower();
+        }
+
+        public string GerarUrl()
+        {
+            string hash = GerarHash();
+
+            string url = "https://trustedcompany.com/api/v1/company/reviews/" + company_url + "?key=" + public_key + "-" + hash + "&page=" + page;
+
+            return url;
+        }
+
+    }
+}
---
+++ Technical test/C#/Test/Test/Question2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace Test
+{
+    public class UniqueLink
+    {
+        int    company_id;
+        string company_secret;
+        string customer_email;
+        string customer_name;
+        string order_id;
+
+        public UniqueLink()
+        {
+            company_id = 564869;
+            company_secret = "a907f5ed930d7d6a6778e60f7fb7acc78f5dc9e2";
+            customer_email = "fcs1983@gmail.com";
+            customer_name = "Fabio";
+            order_id = "xxxx";
+        }
+
+        public UniqueLink(int company_id, string company_secret, string customer_email, string customer_name, string order_id)
+        {
+            this.company_id = company_id;
+            this.company_secret = company_secret;
+            this.customer_email = customer_email;
+            this.customer_name = customer_name;
+            this.order_id = order_id;
+        }
+
+        private string GerarHash()
+        {
+            string encode = company_id + company_secret + customer_email + order_id;
+            ASCIIEncoding AE = new ASCIIEncoding();
+            byte[] HashValue, MessageBytes = AE.GetBytes(encode);
+            SHA1Managed SHhash = new SHA1Managed();
+            string strHex = "";
+            HashValue = SHhash.ComputeHash(MessageBytes);
+            strHex = BitConverter.ToString(HashValue);
+            return strHex;
+        }
+
+        private string GerarUrl()
+        {
+            string hash = GerarHash();
+
+            ASCIIEncoding AE = new ASCIIEncoding();
+            byte[] email_bytes = AE.GetBytes(customer_email);
+            string email_base64 = Convert.ToBase64String(email_bytes);
+
+            string url = "http://trustedcompany.com/review-invite/" + company_id + "/ul/" + hash + "?a=" + email_base64 + "&b=" + customer_name + "&c=" + order_id;
+
+            return HttpUtility.UrlPathEncode(url);
+        }
+    }
+}
